c7ed0c890f30255dd186a9e49c92b4af1acc23e5
l6k <jurcn0@gmail.com>
2023-09-20 17:01:32 +0900

    変更:無限ループ3回でフラグ立つように変更 修正:タイトルのバグ修正

--- Assets/Natsuka/Scripts/ChangeT.cs
+++ Assets/Natsuka/Scripts/ChangeT.cs
@@ -6,6 +6,7 @@ public class ChangeT : MonoBehaviour
 {
 
     public GameObject fpc;
+    private int _flag = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,5 +18,19 @@ public class ChangeT : MonoBehaviour
         Vector3 newPosition = fpc.transform.position;  // 現在の位置をコピー
         newPosition.y += 3f;  // y座標を更新
         fpc.transform.position = newPosition;  // 新しい位置を設定
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _flag += 1;
+            Debug.Log(_flag);
+        }
+        if (_flag == 3)
+        {
+            //無限ループ三回目以降の処理
+            Debug.Log("third loop");
+        }
     }
 }
